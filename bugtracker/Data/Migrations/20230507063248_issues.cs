73dc9e2bcdea539c4dbbbc73d578c7bcacd2423a
Cade Wilson <68790010+cade25wilson@users.noreply.github.com>
2023-05-07 00:40:00 -0600

    Issues table, projectaccess table rename (#2)

---
+++ bugtracker/Data/Migrations/20230507063248_issues.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bugtracker.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class issues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Issues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Issue_Priority = table.Column<int>(type: "int", nullable: false),
+                    CreatorFK = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClosedByFK = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    ClosedOn = table.Column<DateTime>(type: "date", nullable: true),
+                    Resolution = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Issues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Issues_AspNetUsers_ClosedByFK",
+                        column: x => x.ClosedByFK,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Issues_AspNetUsers_CreatorFK",
+                        column: x => x.CreatorFK,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_ClosedByFK",
+                table: "Issues",
+                column: "ClosedByFK");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Issues_CreatorFK",
+                table: "Issues",
+                column: "CreatorFK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Issues");
+        }
+    }
+}
